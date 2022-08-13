65ff66e239892a66d22e9237e09bc81f57e5639b
Kashif Ali <jaxkashif34@gmail.com>
2022-08-13 15:49:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Typography, Button, Tooltip, Collapse, Grow, Fade } from '@mui/material
 export default function App() {
   return (
     <ThemeConfig>
-      <Typography variant="h1">App commponent testing git push and pull request</Typography>
+      <Typography variant="h1">App commponent testing git push and pull request Master Branch</Typography>
 
       <Tooltip title="Add" TransitionComponent={Grow}>
         <Button variant="contained">Click me</Button>
